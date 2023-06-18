da54cb6bd204710ef6b61e3509eba9c68da26c2c
brianyjtai1994 <brianyjtai1994@gmail.com>
2023-06-18 23:34:49 +0800

    Add: 1-Dim. Classic Runge–Kutta method

--- src/SolveDiffEq.jl
+++ src/SolveDiffEq.jl
@@ -1,5 +1,15 @@
 module SolveDiffEq
 
-greet() = print("Hello World!")
+function stepRungeKutta(yₙ::Real, tₙ::Real, h::Real, f::Function)
+    h2 = h / 2
+    h6 = h / 6
+
+    k₁ = f(tₙ,      yₙ)
+    k₂ = f(tₙ + h2, yₙ + h2 * k₁)
+    k₃ = f(tₙ + h2, yₙ + h2 * k₂)
+    k₄ = f(tₙ + h,  yₙ + h  * k₃)
+
+    return yₙ + h6 * (k₁ + 2.0 * k₂ + 2.0 * k₃ + k₄)
+end
 
 end # module SolveDiffEq
