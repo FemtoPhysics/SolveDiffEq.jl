0da2e54315284c741e8f19b033c06a3169fb06d4
brianyjtai1994 <brianyjtai1994@gmail.com>
2023-06-19 03:37:02 +0800

    Add: 1-Dim. Classic Runge–Kutta-Ralston method

--- src/SolveDiffEq.jl
+++ src/SolveDiffEq.jl
@@ -12,4 +12,37 @@ function stepRungeKutta(yₙ::Real, tₙ::Real, h::Real, f::Function)
     return yₙ + h6 * (k₁ + 2.0 * k₂ + 2.0 * k₃ + k₄)
 end
 
+const RKRalstonParams = (
+    αs = (0.0, 0.4, (14 - 3 * SQRT5) / 16, 1.0),
+    β2 = (0.4, 0.0, 0.0, 0.0),
+    β3 = (
+        3 * (476SQRT5 - 963) / 1024,
+        5 * (757 - 324SQRT5) / 1024,
+        0.0, 0.0
+    ),
+    β4 = (
+        (2094SQRT5 - 3365) / 6040,
+        -(3046SQRT5 + 975) / 2552,
+        (203968SQRT5 + 467040) / 240845,
+        0.0
+    ),
+    ws = (
+        (22 - 3SQRT5) / (168 - 36SQRT5),
+        (125SQRT5 - 250) / (180SQRT5 - 456),
+        1024 / (4869SQRT5 - 10038),
+        2SQRT5 / (9SQRT5 + 6)
+    )
+)
+
+function stepRungeKuttaRalston(yₙ::Real, tₙ::Real, h::Real, f::Function)
+    hs = ntuple(i -> RKRalstonParams.αs[i] * h, 4)
+
+    k₁ = h * f(tₙ + hs[1], yₙ)
+    k₂ = h * f(tₙ + hs[2], yₙ + RKRalstonParams.β2[1] * k₁)
+    k₃ = h * f(tₙ + hs[3], yₙ + RKRalstonParams.β3[1] * k₁ + RKRalstonParams.β3[2] * k₂)
+    k₄ = h * f(tₙ + hs[4], yₙ + RKRalstonParams.β4[1] * k₁ + RKRalstonParams.β4[2] * k₂ + RKRalstonParams.β4[3] * k₃)
+
+    return yₙ + RKRalstonParams.ws[1] * k₁ + RKRalstonParams.ws[2] * k₂ + RKRalstonParams.ws[3] * k₃ + RKRalstonParams.ws[4] * k₄
+end
+
 end # module SolveDiffEq
