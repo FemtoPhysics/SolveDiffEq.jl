25c1d3626b89d28588c49ac6e49fcd4ce7682ea7
brianyjtai1994 <brianyjtai1994@gmail.com>
2023-06-19 16:46:18 +0800

    Edit: Runge–Kutta method
    Re-implement RK4 method via Butcher tableau.

--- src/SolveDiffEq.jl
+++ src/SolveDiffEq.jl
@@ -1,48 +1,66 @@
 module SolveDiffEq
 
-function stepRungeKutta(yₙ::Real, tₙ::Real, h::Real, f::Function)
-    h2 = h / 2
-    h6 = h / 6
+const SQRT5 = sqrt(5.0)
 
-    k₁ = f(tₙ,      yₙ)
-    k₂ = f(tₙ + h2, yₙ + h2 * k₁)
-    k₃ = f(tₙ + h2, yₙ + h2 * k₂)
-    k₄ = f(tₙ + h,  yₙ + h  * k₃)
-
-    return yₙ + h6 * (k₁ + 2.0 * k₂ + 2.0 * k₃ + k₄)
+struct ButcherTab{N,Ta,Tb,Tc}
+    mat_a::NTuple{N,NTuple{N,Ta}}
+    vec_b::NTuple{N,Tb}
+    vec_c::NTuple{N,Tc}
 end
 
-const RKRalstonParams = (
-    αs = (0.0, 0.4, (14 - 3 * SQRT5) / 16, 1.0),
-    β2 = (0.4, 0.0, 0.0, 0.0),
-    β3 = (
-        3 * (476SQRT5 - 963) / 1024,
-        5 * (757 - 324SQRT5) / 1024,
-        0.0, 0.0
+const RK4ClassicTab = ButcherTab(
+    (
+        (0.0, 0.0, 0.0, 0.0),
+        (0.5, 0.0, 0.0, 0.0),
+        (0.0, 0.5, 0.0, 0.0),
+        (0.0, 0.0, 1.0, 0.0)
     ),
-    β4 = (
-        (2094SQRT5 - 3365) / 6040,
-        -(3046SQRT5 + 975) / 2552,
-        (203968SQRT5 + 467040) / 240845,
-        0.0
+    (1//6, 1//3, 1//3, 1//6),
+    (0.0, 0.5, 0.5, 1.0)
+)
+
+const RK4RalstonTab = ButcherTab(
+    (
+        (0.0,                       0.0,                       0.0,                             0.0),
+        (0.4,                       0.0,                       0.0,                             0.0),
+        ((1428SQRT5 - 2889) / 1024, (3785 - 1620SQRT5) / 1024, 0.0,                             0.0),
+        ((2094SQRT5 - 3365) / 6040, -(3046SQRT5 + 975) / 2552, (203968SQRT5 + 467040) / 240845, 0.0)
     ),
-    ws = (
+    (
         (22 - 3SQRT5) / (168 - 36SQRT5),
         (125SQRT5 - 250) / (180SQRT5 - 456),
         1024 / (4869SQRT5 - 10038),
         2SQRT5 / (9SQRT5 + 6)
-    )
+    ),
+    (0.0, 0.4, (14 - 3SQRT5) / 16, 1.0)
 )
 
-function stepRungeKuttaRalston(yₙ::Real, tₙ::Real, h::Real, f::Function)
-    hs = ntuple(i -> RKRalstonParams.αs[i] * h, 4)
+@generated function stepRungeKutta(yn::Real, tn::Real, h::Real, f::Function, tab::ButcherTab{N}) where N
+    sym_list = Vector{Symbol}(undef, N)
+    blk_expr = Expr(:block)
+    ret_expr = Expr(:call, :+, :yn)
 
-    k₁ = h * f(tₙ + hs[1], yₙ)
-    k₂ = h * f(tₙ + hs[2], yₙ + RKRalstonParams.β2[1] * k₁)
-    k₃ = h * f(tₙ + hs[3], yₙ + RKRalstonParams.β3[1] * k₁ + RKRalstonParams.β3[2] * k₂)
-    k₄ = h * f(tₙ + hs[4], yₙ + RKRalstonParams.β4[1] * k₁ + RKRalstonParams.β4[2] * k₂ + RKRalstonParams.β4[3] * k₃)
+    push!(blk_expr.args, :(mat_a = tab.mat_a))
+    push!(blk_expr.args, :(vec_b = tab.vec_b))
+    push!(blk_expr.args, :(vec_c = tab.vec_c))
 
-    return yₙ + RKRalstonParams.ws[1] * k₁ + RKRalstonParams.ws[2] * k₂ + RKRalstonParams.ws[3] * k₃ + RKRalstonParams.ws[4] * k₄
+    for n in 1:N
+        sym_list[n] = Symbol(:k, n)
+        a = Expr[]
+        for i in 1:n-1
+            push!(a, :(mat_a[$n][$i] * $(sym_list[i])))
+        end
+        e = Expr(
+            :call, :*, :h, Expr(
+                :call, :f, :(tn + vec_c[$n] * h),
+                Expr(:call, :+, :yn, a...)
+            )
+        )
+        push!(blk_expr.args, :($(sym_list[n]) = @inbounds $e))
+        push!(ret_expr.args, :(vec_b[$n] * $(sym_list[n])))
+    end
+    push!(blk_expr.args, ret_expr)
+    return blk_expr
 end
 
 end # module SolveDiffEq
